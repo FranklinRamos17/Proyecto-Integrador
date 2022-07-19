b56f8d125d6f637ca250811ca87f69055ac3bb94
FranklinRamos17 <javier.quispe.r@upeu.edu.pe>
2022-07-19 16:56:25 -0500

    proyecto

---
+++ Ventas/src/main/java/pe/edu/Upeu/Ventas/App.java
@@ -0,0 +1,13 @@
+package pe.edu.Upeu.Upeu.Ventas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
