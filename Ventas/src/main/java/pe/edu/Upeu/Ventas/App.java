d5ed437129104ef5f621f0398152a32e622b5951
FranklinRamos17 <javier.quispe.r@upeu.edu.pe>
2022-07-19 17:14:56 -0500

    proyecto

--- Ventas/src/main/java/pe/edu/Upeu/Ventas/App.java
+++ Ventas/src/main/java/pe/edu/Upeu/Ventas/App.java
@@ -1,5 +1,7 @@
 package pe.edu.Upeu.Upeu.Ventas;
 
+import pe.edu.upeu.app.MenuOpciones;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       new MenuOpciones().menuPrincipal();
     }
 }
